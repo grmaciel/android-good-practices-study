afb144a99a85a023da1b05ae55d9ba452e062d72
Gilson Maciel <gmaciel@gmail.com>
2015-07-26 19:49:56 -0300

    Added core module.

---
+++ core/src/main/java/br/com/androidcore/activity/NewTestCustomActivity.java
@@ -0,0 +1,9 @@
+package br.com.androidcore.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by Gilson Maciel on 07/04/2015.
+ */
+public abstract class NewTestCustomActivity extends Activity {
+}
