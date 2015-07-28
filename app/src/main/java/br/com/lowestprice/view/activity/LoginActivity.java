a06cdf284e0866c4d3194cabe32bc8618fd1c67c
Gilson Maciel <gmaciel@gmail.com>
2015-07-28 20:50:53 -0300

    Added activity flow and layouts

--- app/src/main/java/br/com/lowestprice/view/activity/LoginActivity.java
+++ app/src/main/java/br/com/lowestprice/view/activity/LoginActivity.java
@@ -1,9 +1,48 @@
 package br.com.lowestprice.view.activity;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.widget.Button;
+
+import br.com.androidcore.activity.BaseActivity;
+import br.com.lowestprice.R;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Gilson Maciel on 26/07/2015.
  */
-public class LoginActivity extends Activity {
+public class LoginActivity extends BaseActivity {
+    @Bind(R.id.loginBtnRegister) Button btnRegisterEmail;
+    @Bind(R.id.loginBtnRegisterFacebook) Button btnRegisterFb;
+    @Bind(R.id.loginBtnLogin) Button btnAccountLogin;
+
+    @Override
+    public void setViewValues() {
+        this.hideActionBar();
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void setViewListener() {
+
+    }
+
+    @Override
+    public int getLayoutFile() {
+        return R.layout.activity_login;
+    }
+
+    @OnClick(R.id.loginBtnLogin)
+    public void actionOnAccountLogin() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.loginBtnRegister)
+    public void actionOnRegister() {
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.right_slide_in, R.anim.left_slide_out);
+    }
 }
--- app/src/main/java/br/com/lowestprice/view/activity/MainActivity.java
+++ app/src/main/java/br/com/lowestprice/view/activity/MainActivity.java
@@ -1,9 +1,25 @@
 package br.com.lowestprice.view.activity;
 
-import android.app.Activity;
+import br.com.androidcore.activity.BaseActivity;
+import br.com.lowestprice.R;
 
 /**
  * Created by Gilson Maciel on 26/07/2015.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends BaseActivity {
+
+
+    @Override
+    public void setViewValues() {
+    }
+
+    @Override
+    public void setViewListener() {
+
+    }
+
+    @Override
+    public int getLayoutFile() {
+        return R.layout.activity_main;
+    }
 }
