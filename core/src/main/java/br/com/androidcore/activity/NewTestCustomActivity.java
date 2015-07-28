600d566b020aa3d0527b0dbbc69684c8c9d8516b
Gilson Maciel <gmaciel@gmail.com>
2015-07-28 20:05:51 -0300

    Removido core do projeto e adicionado referência a biblioteca.

--- core/src/main/java/br/com/androidcore/activity/NewTestCustomActivity.java
+++
@@ -1,9 +0,0 @@
-package br.com.androidcore.activity;
-
-import android.app.Activity;
-
-/**
- * Created by Gilson Maciel on 07/04/2015.
- */
-public abstract class NewTestCustomActivity extends Activity {
-}
