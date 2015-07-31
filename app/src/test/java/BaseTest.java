4a84c048c00f8ad9366b1467a88c6fec207a051f
Gilson Maciel <gmaciel@gmail.com>
2015-07-30 21:14:09 -0300

    Changed build and tests

--- app/src/test/java/BaseTest.java
+++ app/src/test/java/BaseTest.java
@@ -1,10 +1,14 @@
+import android.app.Activity;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
 import br.com.lowestprice.BuildConfig;
+import br.com.lowestprice.view.activity.LoginActivity;
 
 /**
  * Created by Gilson Maciel on 18/07/2015.
@@ -17,4 +21,8 @@ public abstract class BaseTest {
 
     @After
     public abstract void tearDown();
+
+    public <T extends Activity> T getActivity(Class<T> activityClass) {
+        return Robolectric.setupActivity(activityClass);
+    }
 }
