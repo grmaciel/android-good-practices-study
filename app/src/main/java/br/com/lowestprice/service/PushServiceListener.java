4e1d436e26123dd57a3c2930f6e2e6b416a748e9
Gilson Maciel <gmaciel@gmail.com>
2015-08-24 21:32:22 -0300

    Removed repeated icon

--- app/src/main/java/br/com/lowestprice/service/PushServiceListener.java
+++ app/src/main/java/br/com/lowestprice/service/PushServiceListener.java
@@ -37,7 +37,7 @@ public class PushServiceListener extends GcmListenerService {
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_launcher)
+                .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("GCM Message")
                 .setContentText(message)
                 .setAutoCancel(true)
